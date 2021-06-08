da6fbd7cd0d69245e9e3ff57c365d7a8d270901c
r121212-Iwama <75057420+r121212-Iwama@users.noreply.github.com>
2021-06-08 15:15:32 +0900

    OumuBOT

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, abort
+#環境変数取得用
+import os
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,9 +11,12 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-import os
 
 app = Flask(__name__)
+
+# オリジナルの処理
+# line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+# handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
@@ -19,6 +24,10 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -46,6 +55,4 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
